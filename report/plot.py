c3309d40eb0443e68f5f78237fb00c7af54c7ee0
Tfloow <88899420+Tfloow@users.noreply.github.com>
2024-12-04 13:06:16 +0100

    Updated drawing

--- report/plot.py
+++ report/plot.py
@@ -106,3 +106,51 @@ fig.tight_layout()
 
 plt.savefig("montgomery_report_perf.pdf")
 #plt.show()
+
+### RSA ###
+# REDO IMPLEMENTATION TO FIND THE RIGHT DATA
+ite = [1,2]
+Cycle_montgomery = [3097, 3097]
+Cycle = [100000, 100000] # for 16 bit exponents
+WNS = [-0.044, 0.07]
+LUTS = [8289, 9977]
+REG = [7251, 7234]
+
+fig, axs = plt.subplots(1,2,figsize=(10, 4))
+
+ax1 = axs[0]
+ax2 = axs[1]
+
+ax1.set_title("Speed of the Montgomery Multiplier")
+ax1.plot(ite,Cycle_montgomery,"-o", label="# of Cycles Adder", color=color1)
+ax1.plot(ite,Cycle,"-s", label="# of Cycles Montgomery", color=color1)
+
+ax1_twin = ax1.twinx()
+ax1_twin.plot(ite,WNS,"--v", label="Worst Negative Slack", color=color1)
+
+ax1.set_xlabel("Iteration of the Design")
+ax1.set_ylabel("Cycles")
+ax1_twin.set_ylabel("Worst Negative Slack [ns]")
+ax1.set_xlim(0,6)
+ax1_twin.set_ylim(-2.5,2.5)
+ax1_twin.hlines(y=0,xmin=0,xmax=6, linestyles="--", alpha=0.5, color="grey")
+ax1.grid()
+
+ax2.set_title("Utilization of the Montgomery Multiplier")
+ax2.plot(ite, LUTS, "-^", label="# of Sliced LUTS", color=color2)
+ax2.plot(ite, REG,  "-s", label="# of Sliced Registers", color=color2)
+ax2.set_ylim(0,30000)
+
+ax2.set_xlabel("Iteration of the Design")
+ax2.set_ylabel("Amount")
+ax2.set_xlim(0,6)
+ax2.grid()
+
+handles1, labels1 = ax1.get_legend_handles_labels()
+handles2, labels2 = ax1_twin.get_legend_handles_labels()
+ax1.legend(handles1 + handles2, labels1 + labels2)
+
+ax2.legend()
+fig.tight_layout()
+
+plt.savefig("rsa_report_perf.pdf")
--- rsa/rsa.v
+++ rsa/rsa.v
@@ -160,7 +160,12 @@ module rsa (
     assign in_a = (isX_TildeMM_SAVE || isR_2NMM_SAVE) ? X_tilde_Q : A_Q;
     assign in_b = isDMAMM ? in_a : (isOne ? 1024'h1 : (isR_2NMM_SAVE ? R2_N_Q : X_tilde_Q)); 
     assign in_m = N_Q;
+<<<<<<< Updated upstream
     assign dma_tx_data = result; // to avoid over writing
+=======
+    assign dma_tx_data = done_montgomery ? result : 1024'h0; // to avoid over writing
+    // NEED TO CHECK IF THIS PRECAUTION IS IMPORTANT ???
+>>>>>>> Stashed changes
     // I MAY USE TOOOOOO MANY LUTS LOL
   
   // command to check if receiving save data
